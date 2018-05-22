8ce539e80c8a80979cc23c7df9fff4a50206e440
iktakahiro <takahiro.ikeuchi@gmail.com>
2018-05-22 22:02:28 +0900

    Add activate and deactivate methods

--- contracts/ProofOfRead.sol
+++ contracts/ProofOfRead.sol
@@ -9,45 +9,45 @@ contract ProofOfRead {
     }
 
     struct Book {
-        bytes32 isbn;
+        bytes32 isbn13;
         Author[] authors;
     }
 
-    Book public book;
+    bool public isActive;
     mapping(bytes32 => Author) public books;
 
-    event Logging (bytes32 _isbn, address reader, uint8 score);
+    event Logging (bytes32 _isbn13, address reader, uint8 score);
 
-    function ProofOfRead(bytes32 _isbn, address[] _addressList, uint[] _loyaltyList) {
-        // constructor
-        book.isbn = _isbn;
-
-        // TODO validate isbn code
-        require(isEqualLength(_addressList, _loyaltyList));
-
-        for (var i = 0; i < _addressList.length; i++) {
-            book.authors.push(Author({addr : _addressList[i], loyalty : _loyaltyList[i]}));
-        }
+    function ProofOfRead(bool _isActive) public {
+        isActive = _isActive;
     }
 
-    function addBook(bytes32 _isbn, address[] _addressList, uint[] _loyaltyList) public {
+    function activateContract() public {
+        isActive = true;
+    }
+
+    function deactivateContract() public {
+        isActive = false;
+    }
+
+    function addBook(bytes32 _isbn13, address[] _addressList, uint[] _loyaltyList) public {
         // TODO ownerble
 
         require(isEqualLength(_addressList, _loyaltyList));
-        require(isValidIsbn(_isbn));
+        require(isValidIsbn(_isbn13));
 
         Author authors;
         for (var i = 0; i < _addressList.length; i++) {
             authors.push(Author({addr : _addressList[i], loyalty : _loyaltyList[i]}));
         }
 
-        books[_isbn] = authors;
+        books[_isbn13] = authors;
     }
 
-    function getAuthors(bytes32 _isbn) public view returns (address[], uint8) {
+    function getAuthors(bytes32 _isbn13) public view returns (address[], uint8) {
         address[] _addressList;
         uint8[] _loyaltyList;
-        _book = books[_isbn];
+        _book = books[_isbn13];
 
         for (var i = 0; i < _book.authors.length; i++) {
             _addressList.push(_book.authors[i].addr);
@@ -56,33 +56,35 @@ contract ProofOfRead {
         return (_addressList, _loyaltyList);
     }
 
-    function recordReadingHistory(bytes32 _isbn, uint8 score) public payable {
+    function recordReadingHistory(bytes32 _isbn13, uint8 score) public payable {
+        assert(isActive);
+
         require(isValidScore(_score));
-        require(isIsbnExists(_isbn));
+        require(isIsbnExists(_isbn13));
 
         // TODO split ether
-        books[_isbn].authors[0].transfer(msg.value);
+        books[_isbn13].authors[0].transfer(msg.value);
 
-        Logging(_isbn, msg.sender, score);
+        Logging(_isbn13, msg.sender, score);
     }
 
-    function isEqualLength(address[] a1, uint8[] a2) private view returns (bool) {
+    function isEqualLength(address[] a1, uint8[] a2) private pure returns (bool) {
         // Return whether the lengths of two arrays are equal.
         return (a1.length == a2.length);
     }
 
-    function isValidIsbn(bytes32 _isbn) private view returns (bool) {
+    function isValidIsbn(bytes32 _isbn13) private pure returns (bool) {
         // Return whether a ISBN is valid.
         // TODO need to implement
         return true;
     }
 
-    function isIsbnExists(bytes32 _isbn) private view returns (bool) {
+    function isIsbnExists(bytes32 _isbn13) private pure returns (bool) {
         // Return whether a ISBN exists.
-        return books[_isbn];
+        return books[_isbn13];
     }
 
-    function isValidScore(uint8 _score) private view returns (bool) {
+    function isValidScore(uint8 _score) private pure returns (bool) {
         // Return whether a score is valid. It should be between 1 and 5.
         if (_score >= 1 && _score <= 5) {
             return true;
