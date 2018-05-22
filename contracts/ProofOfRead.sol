bc533b429dc3b3e0320dbfe3cc929699230cc873
iktakahiro <takahiro.ikeuchi@gmail.com>
2018-05-22 22:27:07 +0900

    Add changeMaxLengthOfAuthors()

--- contracts/ProofOfRead.sol
+++ contracts/ProofOfRead.sol
@@ -3,45 +3,56 @@ pragma solidity 0.4.23;
 
 contract ProofOfRead {
 
+    // Author of a Book
     struct Author {
         address addr;
         uint loyalty;
     }
 
+    // Book
     struct Book {
         bytes32 isbn13;
         Author[] authors;
     }
 
     bool public isActive;
+    uint8 private maxLengthOfAuthors;
+
     mapping(bytes32 => Author) public books;
 
     event Logging (bytes32 _isbn13, address reader, uint8 score);
 
     function ProofOfRead(bool _isActive) public {
         isActive = _isActive;
+        maxLengthOfAuthors = 6;
     }
 
     function activateContract() public {
+        // TODO ownerble
         isActive = true;
     }
 
     function deactivateContract() public {
+        // TODO ownerble
         isActive = false;
+    }
+
+    function changeMaxLengthOfAuthors(uint8 _maxLength) public {
+        // TODO ownerble
+        maxLengthOfAuthors = _maxLength;
     }
 
     function addBook(bytes32 _isbn13, address[] _addressList, uint[] _loyaltyList) public {
         // TODO ownerble
 
-        require(isEqualLength(_addressList, _loyaltyList));
         require(isValidIsbn(_isbn13));
+        require(_addressList.length <= maxLengthOfAuthors);
+        require(isEqualLength(_addressList, _loyaltyList));
+        require(isTotal100(_loyaltyList));
 
-        Author authors;
         for (var i = 0; i < _addressList.length; i++) {
-            authors.push(Author({addr : _addressList[i], loyalty : _loyaltyList[i]}));
+            books[_isbn13].authors.push(Author({addr : _addressList[i], loyalty : _loyaltyList[i]}));
         }
-
-        books[_isbn13] = authors;
     }
 
     function removeBook(bytes32 _isbn13) public {
@@ -54,8 +65,13 @@ contract ProofOfRead {
     }
 
     function getAuthors(bytes32 _isbn13) public view returns (address[], uint8) {
-        address[] _addressList;
-        uint8[] _loyaltyList;
+
+        require(isIsbnExists(_isbn13));
+
+        address[] memory _addressList;
+        uint8[] memory _loyaltyList;
+        bytes32 memory _book;
+
         _book = books[_isbn13];
 
         for (var i = 0; i < _book.authors.length; i++) {
@@ -75,6 +91,18 @@ contract ProofOfRead {
         books[_isbn13].authors[0].transfer(msg.value);
 
         Logging(_isbn13, msg.sender, score);
+    }
+
+    function isTotal100(uint8[] a) private pure returns (bool) {
+        // Return whether the result of a calculation equal 100.
+        uint8 memory _total;
+
+        _total = 0;
+        for (var i = 0; i < a.length; i++) {
+            _total = _total + a;
+        }
+
+        return (_total == 100);
     }
 
     function isEqualLength(address[] a1, uint8[] a2) private pure returns (bool) {
